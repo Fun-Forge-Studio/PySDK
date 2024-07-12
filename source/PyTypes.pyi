4e3a7f01dc4de847d8b4b5d165457d1156d0fc9d
Aaron Thompson <142937112+DevByEagle@users.noreply.github.com>
2024-07-12 17:10:48 -0400

    Update PyTypes.pyi

--- source/PyTypes.pyi
+++ source/PyTypes.pyi
@@ -2,11 +2,17 @@ from typing import overload
 from typing_extensions import Self
 
 class Boolean:
+  @property
   def true(self) -> int:
     return 1
-
-  def false(self) -> str:
+  @property
+  def false(self) -> int:
     return -1
-  
-  def Switch(self):
-    pass
+
+  def Switch(self, o: object) -> int | None:
+    if o:
+      return self.false
+    elif not o:
+      return self.true
+    else:
+      return None
